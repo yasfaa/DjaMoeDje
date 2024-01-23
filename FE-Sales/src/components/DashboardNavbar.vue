0b61085871a8ab8239849f83305c7cd337938a11
yasfaa <yasfanaruto@gmail.com>
2024-01-23 10:53:15 +0700

    fix sidebar FE

--- FE-Sales/src/components/DashboardNavbar.vue
+++ FE-Sales/src/components/DashboardNavbar.vue
@@ -1,152 +1,126 @@
 <template>
-  <main>
-    <Navbar />
-    <section class="navbar-section">
-      <div class="container">
-        <div class="form-box" :class="{ 'register-form': isRegister }">
-          <div class="padding-container">
-              <div v-if="role === 'Admin'">
-                <div class="transition-content" :class="{ pushMainContent: isActive }">
-                  <div>
-                    <div id="mySidenav" class="sidenav shadow" :class="{ openNavClass: isActive }">
-                      <a class="closebtn" @click="isActive = !isActive" style="cursor: pointer"
-                        >&times;</a
-                      >
-                      <a href="/admin/dashboard">Home</a>
-                      <a href="/admin/daftarbuku">Daftar Buku</a>
-                      <a href="/admin/kelolapelanggan">Kelola Pelangan</a>
-                    </div>
-                    <div class="content">
-                      <div class="button-side">
-                        <nav class="navbar navbar-expand-lg navbar-light white shadow-sm bg-light fixed-top">
-                          <span
-                            style="font-size: 25px; cursor: pointer"
-                            @click="isActive = !isActive"
-                            >&#9776;
-                          </span>
-                          <a class="navbar-brand" href="/" style="font-size: 32px;">DjaMoeDje</a>
-                          <div class="ms-auto mx-2">
-                            <button
-                              class="navbar-toggler"
-                              type="button"
-                              data-bs-toggle="collapse"
-                              data-bs-target="#navbarSupportedContent"
-                              aria-controls="navbarSupportedContent"
-                              aria-expanded="false"
-                              aria-label="Toggle navigation"
+    <div class="transition-content" :class="{ pushMainContent: isActive }">
+      <div v-if="role === 'Admin'">
+        <div id="mySidenav" class="sidenav shadow" :class="{ openNavClass: isActive }">
+          <a class="closebtn" @click="isActive = !isActive" style="cursor: pointer">&times;</a>
+          <a href="/admin/dashboard">Home</a>
+          <a href="/admin/daftarbuku">Daftar Buku</a>
+          <a href="/admin/kelolapelanggan">Kelola Pelangan</a>
+        </div>
+        <div class="content">
+          <div class="button-side">
+            <nav class="navbar navbar-expand-lg navbar-light white shadow-sm bg-light fixed-top">
+              <span style="font-size: 25px; cursor: pointer" @click="isActive = !isActive"
+                >&#9776;
+              </span>
+              <a class="navbar-brand" href="/" style="font-size: 32px">DjaMoeDje</a>
+              <div class="ms-auto mx-2">
+                <button
+                  class="navbar-toggler"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#navbarSupportedContent"
+                  aria-controls="navbarSupportedContent"
+                  aria-expanded="false"
+                  aria-label="Toggle navigation"
+                >
+                  <ion-icon name="person"></ion-icon>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                  <div class="div">
+                    <ul class="navbar-nav mb-2 mb-lg-0">
+                      <li class="nav-item dropdown">
+                        <button
+                          class="btn dropdown-toggle"
+                          data-bs-toggle="dropdown"
+                          aria-expanded="false"
+                        >
+                          {{ username }}
+                        </button>
+                        <ul class="dropdown-menu dropdown-menu-end">
+                          <li>
+                            <a class="dropdown-item" @click="profile"
+                              ><ion-icon name="person"></ion-icon>&nbsp;Profile</a
                             >
-                              <ion-icon name="person"></ion-icon>
-                            </button>
-                            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                              <div class="div">
-                                <ul class="navbar-nav mb-2 mb-lg-0">
-                                  <li class="nav-item dropdown">
-                                    <button
-                                      class="btn dropdown-toggle"
-                                      data-bs-toggle="dropdown"
-                                      aria-expanded="false"
-                                    >
-                                      {{ username }}
-                                    </button>
-                                    <ul class="dropdown-menu dropdown-menu-end">
-                                      <li>
-                                        <a class="dropdown-item" @click="profile"
-                                          ><ion-icon name="person"></ion-icon>&nbsp;Profile</a
-                                        >
-                                      </li>
-                                      <li>
-                                        <a
-                                          class="dropdown-item"
-                                          @click="onLogout()"
-                                          style="cursor: pointer"
-                                          ><ion-icon name="log-out"></ion-icon>&nbsp;Logout</a
-                                        >
-                                      </li>
-                                    </ul>
-                                  </li>
-                                </ul>
-                              </div>
-                            </div>
-                          </div>
-                        </nav>
-                      </div>
-                      <slot></slot>
-                    </div>
+                          </li>
+                          <li>
+                            <a class="dropdown-item" @click="onLogout()" style="cursor: pointer"
+                              ><ion-icon name="log-out"></ion-icon>&nbsp;Logout</a
+                            >
+                          </li>
+                        </ul>
+                      </li>
+                    </ul>
                   </div>
                 </div>
               </div>
-              <div v-else>
-                <div class="transition-content" :class="{ pushMainContent: isActive }">
-                  <div>
-                    <div id="mySidenav" class="sidenav shadow" :class="{ openNavClass: isActive }">
-                      <a class="closebtn" @click="isActive = !isActive" style="cursor: pointer"
-                        >&times;</a
-                      >
-                      <a href="/dashboard">Home</a>
-                      <a href="/menu">Daftar Menu</a>
-                      <a href="/Pesanan">Pesanan Saya</a>
-                    </div>
-                    <div class="content">
-                      <div class="button-side">
-                        <nav class="navbar navbar-expand-lg navbar-light white shadow-sm bg-light fixed-top">
-                        <span style="font-size: 25px; cursor: pointer" @click="isActive = !isActive">&#9776;
-                          </span>
-                          <a class="navbar-brand" href="/" style="font-size: 32px;">DjaMoeDje</a>
-                          <div class="ms-auto mx-2">
-                            <button
-                              class="navbar-toggler"
-                              type="button"
-                              data-bs-toggle="collapse"
-                              data-bs-target="#navbarSupportedContent"
-                              aria-controls="navbarSupportedContent"
-                              aria-expanded="false"
-                              aria-label="Toggle navigation"
-                            >
-                              <ion-icon name="person"></ion-icon>
-                            </button>
-                            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                              <div class="div">
-                                <ul class="navbar-nav mb-2 mb-lg-0">
-                                  <li class="nav-item dropdown">
-                                    <button
-                                      class="btn dropdown-toggle"
-                                      data-bs-toggle="dropdown"
-                                      aria-expanded="false"
-                                    >
-                                      {{ username }}
-                                    </button>
-                                    <ul class="dropdown-menu dropdown-menu-end">
-                                      <li>
-                                        <a class="dropdown-item" @click="profile"
-                                          ><ion-icon name="person"></ion-icon>&nbsp;Profile</a
-                                        >
-                                      </li>
-                                      <li>
-                                        <a
-                                          class="dropdown-item"
-                                          @click="onLogout()"
-                                          style="cursor: pointer"
-                                          ><ion-icon name="log-out"></ion-icon>&nbsp;Logout</a
-                                        >
-                                      </li>
-                                    </ul>
-                                  </li>
-                                </ul>
-                              </div>
-                            </div>
-                          </div>
-                        </nav>
-                      </div>
-                      <slot></slot>
-                    </div>
-                  </div>
-                </div>
-              </div>
+            </nav>
           </div>
+          <slot></slot>
         </div>
       </div>
-    </section>
-  </main>
+      <div v-else>
+            <div id="mySidenav" class="sidenav shadow" :class="{ openNavClass: isActive }">
+              <a class="closebtn" @click="isActive = !isActive" style="cursor: pointer">&times;</a>
+              <a href="/dashboard">Home</a>
+              <a href="/menu">Daftar Menu</a>
+              <a href="/Pesanan">Pesanan Saya</a>
+            </div>
+            <div class="content">
+              <div class="button-side">
+                <nav
+                  class="navbar navbar-expand-lg navbar-light white shadow-sm bg-light fixed-top"
+                >
+                  <span style="font-size: 25px; cursor: pointer" @click="isActive = !isActive"
+                    >&#9776;
+                  </span>
+                  <a class="navbar-brand" href="/" style="font-size: 32px">DjaMoeDje</a>
+                  <div class="ms-auto mx-2">
+                    <button
+                      class="navbar-toggler"
+                      type="button"
+                      data-bs-toggle="collapse"
+                      data-bs-target="#navbarSupportedContent"
+                      aria-controls="navbarSupportedContent"
+                      aria-expanded="false"
+                      aria-label="Toggle navigation"
+                    >
+                      <ion-icon name="person"></ion-icon>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                      <div class="div">
+                        <ul class="navbar-nav mb-2 mb-lg-0">
+                          <li class="nav-item dropdown">
+                            <button
+                              class="btn dropdown-toggle"
+                              data-bs-toggle="dropdown"
+                              aria-expanded="false"
+                            >
+                              {{ username }}
+                            </button>
+                            <ul class="dropdown-menu dropdown-menu-end">
+                              <li>
+                                <a class="dropdown-item" @click="profile"
+                                  ><ion-icon name="person"></ion-icon>&nbsp;Profile</a
+                                >
+                              </li>
+                              <li>
+                                <a class="dropdown-item" @click="onLogout()" style="cursor: pointer"
+                                  ><ion-icon name="log-out"></ion-icon>&nbsp;Logout</a
+                                >
+                              </li>
+                            </ul>
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                </nav>
+              </div>
+              <slot></slot>
+            </div>
+          </div>
+        </div>
 </template>
 
 <script>
@@ -191,169 +165,128 @@ export default {
     //   this.$router.push({ path: `/profile/${id}` })
     // }
     onLogout() {
-        axios.post(BASE_URL + '/auth/logout', {}, {
+      axios
+        .post(
+          BASE_URL + '/auth/logout',
+          {},
+          {
             headers: {
-                Authorization: "Bearer " + localStorage.getItem('access_token'),
+              Authorization: 'Bearer ' + localStorage.getItem('access_token')
             }
+          }
+        )
+        .then((response) => {
+          localStorage.removeItem('access_token')
+          this.$router.push('/login')
         })
-            .then(response => {
-                localStorage.removeItem('access_token');
-                this.$router.push('/login');
-            })
-            .catch(error => {
-                console.error(error);
-            });
+        .catch((error) => {
+          console.error(error)
+        })
     }
   }
 }
 </script>
 <style scoped>
-.navbar-section {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    min-height: 100vh;
+
+.navbar {
+    padding-left: 15px;
+}
+
+.navbar-brand {
+    font-weight: bold;
+    padding-left: 15px;
+}
+
+.sidenav {
+    height: 100%;
+    width: 0;
+    position: fixed;
+    z-index: 1;
+    top: 0;
+    left: -250px;
+    background-color: #4b4b4b;
+    overflow-x: hidden;
+    transition: 0.5s;
+    padding-top: 80px;
+    width: 250px;
+}
+
+.openNavClass {
+    left: 0;
+}
+
+.transition-content {
+    transition: margin-left .5s;
+}
+
+.pushMainContent {
+    margin-left: 250px;
+}
+
+@media screen and (max-width: 768px) {
+    .pushMainContent {
+        margin-left: 0;
+    }
+}
+
+.sidenav a {
+    padding: 8px 8px 8px 32px;
+    text-decoration: none;
+    font-size: 18px;
+    color: #ffffff;
+    display: block;
+    transition: 0.3s;
+}
+
+.sidenav a:hover {
+    color: #f1f1f1;
+}
+
+.sidenav .closebtn {
+    position: absolute;
+    top: 0;
+    right: 25px;
+    font-size: 36px;
+    margin-left: 50px;
+}
+
+.button-side {
+    padding: 16px;
+
+}
+
+.overlay {
+    position: fixed;
+    top: 0;
+    left: 0;
     width: 100%;
-    background-image: url('../assets/LandingPage/Background.png');
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.6);
+    /* Change the alpha value to adjust transparency */
+    display: none;
+    z-index: 0;
+}
+
+.showOverlay {
+    display: block;
+    z-index: 1;
+}
+
+@media screen and (max-height: 450px) {
+    .sidenav {
+        padding-top: 15px;
+    }
+
+    .sidenav a {
+        font-size: 18px;
+    }
+}
+
+.content {
+    min-height: 100vh;
+
+    background: url("../../../src/assets/LandingPage/Background.png");
     background-position: center;
     background-size: cover;
 }
-
-.form-box {
-    position: relative;
-    width: 400px;
-    height: 450px;
-    background: transparent;
-    border: 2px solid rgba(0, 0, 0, 0.5);
-    border-radius: 20px;
-    backdrop-filter: blur(15px);
-    display: flex;
-    justify-content: center;
-    align-items: center;
-
-}
-
-h2 {
-    font-size: 2em;
-    color: black;
-    text-align: center;
-}
-
-.inputbox {
-    position: relative;
-    margin: 30px 0;
-    width: 310px;
-    border-bottom: 2px solid black;
-}
-
-.inputbox label {
-    position: absolute;
-    top: 50%;
-    left: 5px;
-    transform: translateY(-50%);
-    color: black;
-    font-size: 1em;
-    pointer-events: none;
-    transition: .5s;
-}
-
-.input-type:focus~label,
-.input-type:valid~label {
-    top: -5px;
-}
-
-.inputbox input {
-    width: 100%;
-    height: 50px;
-    background: transparent;
-    border: none;
-    outline: none;
-    font-size: 1em;
-    padding: 0 35px 0 5px;
-    color: black;
-}
-
-.inputbox ion-icon {
-    position: absolute;
-    right: 8px;
-    color: black;
-    font-size: 1.2em;
-    top: 20px;
-}
-
-.forget {
-    margin: -15px 0 15px;
-    font-size: .9em;
-    color: black;
-    display: flex;
-    justify-content: space-between;
-}
-
-.forget label input {
-    margin-right: 3px;
-
-}
-
-.forget label a {
-    color: black;
-    text-decoration: none;
-}
-
-.forget label a:hover {
-    text-decoration: underline;
-}
-
-.login-button {
-    width: 100%;
-    height: 40px;
-    border-radius: 40px;
-    background: black;
-    border: none;
-    outline: none;
-    cursor: pointer;
-    font-size: 1em;
-    font-weight: 600;
-}
-
-.register {
-    font-size: .9em;
-    color: black;
-    text-align: center;
-    margin: 25px 0 10px;
-}
-
-.register p a {
-    text-decoration: none;
-    color: black;
-    font-weight: 600;
-}
-
-.register p a:hover {
-    text-decoration: underline;
-}
-
-.container {
-    padding: 20px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-}
-
-.padding-container {
-    padding: 20px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-}
-
-.fade-enter-active,
-.fade-leave-active {
-    transition: opacity 0.5s;
-}
-
-.fade-enter,
-.fade-leave-to {
-    opacity: 0;
-}
 </style>
-
