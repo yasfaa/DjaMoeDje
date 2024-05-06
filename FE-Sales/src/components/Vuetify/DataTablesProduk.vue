d77aaea74c77672afda7885c3270e79c9eb1820d
yasfaa <yasfanaruto@gmail.com>
2024-05-06 19:40:42 +0700

    fix admin datatables

--- FE-Sales/src/components/Vuetify/DataTablesProduk.vue
+++ FE-Sales/src/components/Vuetify/DataTablesProduk.vue
@@ -1,43 +1,55 @@
 <template>
   <v-card flat>
     <v-card-title class="d-flex align-center pe-2">
-      <v-icon icon="mdi-video-input-component"></v-icon> &nbsp; Daftar Menu
-
+       Daftar Menu
       <v-spacer></v-spacer>
-
       <v-text-field
         v-model="search"
         density="compact"
         label="Search"
-        prepend-inner-icon="mdi-magnify"
         variant="solo-filled"
         flat
         hide-details
         single-line
       ></v-text-field>
+      <div class="button-set mx-4">
+        <v-btn
+          color="#b9a119"
+          rounded="l"
+          data-bs-toggle="modal"
+          data-bs-target="#addMenu"
+          class=""
+        >
+          Tambah Menu
+        </v-btn>
+      </div>
     </v-card-title>
 
     <v-divider></v-divider>
+    
     <v-data-table v-model:search="search" :items="menus">
-      <template v-slot:item.imagePath="{ item }">
-        <img
-          :src="item.imagePath ? item.imagePath : 'https://via.placeholder.com/150'"
-          height="150"
-          alt="Menu Image"
-        />
+      <template v-slot:item.gambar="{ item }">
+        <v-row>
+          <v-col v-for="(gambar, index) in item.gambar" :key="index" cols="12" sm="6" md="4" lg="3" class="mx-1">
+            <img
+              :src="gambar ? gambar : 'https://via.placeholder.com/150'"
+              height="150"
+              alt="Menu Image"
+            />
+          </v-col>
+        </v-row>
       </template>
 
-      <template v-slot:item.total="{ item }">
-        <div class="text-start">Rp{{ item.total }}</div>
+      <template v-slot:item.nama="{ item }">
+        <div class="text-start">{{ item.nama }}</div>
+      </template>
+
+      <template v-slot:item.harga="{ item }">
+        <div class="text-start">Rp{{ item.harga }}</div>
       </template>
 
       <template v-slot:item.deskripsi="{ item }">
         <div>{{ item.deskripsi }}</div>
-      </template>
-
-      <template v-slot:item.actions="{ item }">
-        <v-btn small color="blue" text @click="goToMenu(item.id)">View</v-btn>
-        <v-btn small color="blue" text>Edit</v-btn>
       </template>
     </v-data-table>
   </v-card>
@@ -58,7 +70,14 @@ const retrieveMenus = async () => {
         Authorization: 'Bearer ' + localStorage.getItem('access_token')
       }
     })
-    menus.value = response.data.menus
+    menus.value = response.data.menus.map((menu) => {
+      return {
+        nama: menu.nama,
+        harga: menu.total,
+        deskripsi: menu.deskripsi,
+        gambar: menu.imagePaths 
+      }
+    })
   } catch (error) {
     console.error('Error fetching menus:', error)
   }
@@ -72,5 +91,3 @@ const goToMenu = (menuId) => {
   this.$router.push(`/menu/${menuId}`)
 }
 </script>
-
-<style></style>
